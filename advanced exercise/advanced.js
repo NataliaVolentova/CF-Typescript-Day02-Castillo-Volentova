71754737005a5d3f5086886f7d82a9d08a781ac3
Robert Castillo <robert.castillo@gmx.at>
2020-10-14 13:10:13 +0200

    advanced exercise part 1

---
+++ advanced exercise/advanced.js
@@ -0,0 +1,15 @@
+var Vehicles = /** @class */ (function () {
+    function Vehicles(price, model, horsePower, yearsOfProduction) {
+        this.price = "";
+        this.model = "";
+        this.horsePower = "";
+        this.yearsOfProduction = "";
+        this.price = price;
+        this.model = model;
+        this.horsePower = horsePower;
+        this.yearsOfProduction;
+    }
+    return Vehicles;
+}());
+$(document).ready(function () {
+});
---
+++ advanced exercise/advanced.ts
@@ -0,0 +1,19 @@
+class Vehicles {
+    price = "";
+    model = "";
+    horsePower = "";
+    yearsOfProduction = "";
+
+    constructor(price, model, horsePower, yearsOfProduction){
+        this.price = price;
+        this.model = model;
+        this.horsePower = horsePower;
+        this.yearsOfProduction;
+    }
+}
+
+$(document).ready(function(){
+
+})
+
+    

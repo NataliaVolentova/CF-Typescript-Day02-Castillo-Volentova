8750d69f13551e020184836be4790d85500c0f4e
Natalia <nvolentova@gmail.com>
2020-10-14 15:07:37 +0200

    advanced

--- advanced exercise/advanced.js
+++ advanced exercise/advanced.js
@@ -1,15 +1,43 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Vehicles = /** @class */ (function () {
     function Vehicles(price, model, horsePower, yearsOfProduction) {
-        this.price = "";
-        this.model = "";
-        this.horsePower = "";
-        this.yearsOfProduction = "";
         this.price = price;
         this.model = model;
         this.horsePower = horsePower;
-        this.yearsOfProduction;
+        this.yearsOfProduction = yearsOfProduction;
     }
+    Vehicles.prototype.calculation = function () {
+        this.price = this.horsePower * 2 + (2020 - this.yearsOfProduction) * 1.4;
+        console.log("The price of " + this.model + " is: " + this.price + " ");
+        return this.price;
+    };
     return Vehicles;
 }());
-$(document).ready(function () {
-});
+var volvo = new Vehicles(15000, "volvo", 150, 1999);
+console.log(volvo.calculation());
+var calculationResult = document.getElementById("result-mb"); /*as HTMLInputElement;*/
+calculationResult.innerHTML = volvo.calculation();
+var Motorbike = /** @class */ (function (_super) {
+    __extends(Motorbike, _super);
+    function Motorbike(price, model, horsePower, yearsOfProduction, numberWheel) {
+        var _this = _super.call(this, price, model, horsePower, yearsOfProduction) || this;
+        _this.numberWheel = "";
+        _this.numberWheel = numberWheel;
+        return _this;
+    }
+    return Motorbike;
+}(Vehicles));
+/*document.getElementsByClassName("result-mb").innerHTML(`The price of ${this.model} is: ${this.price} `);*/
+$;
--- advanced exercise/advanced.ts
+++ advanced exercise/advanced.ts
@@ -1,19 +1,45 @@
 class Vehicles {
-    price = "";
-    model = "";
-    horsePower = "";
-    yearsOfProduction = "";
+    price : number;
+    model : string;
+    horsePower : number;
+    yearsOfProduction : number;
 
-    constructor(price, model, horsePower, yearsOfProduction){
+    constructor(price: number, model: string, horsePower: number, yearsOfProduction: number){
         this.price = price;
         this.model = model;
         this.horsePower = horsePower;
-        this.yearsOfProduction;
+        this.yearsOfProduction = yearsOfProduction;
+    }
+    calculation(){
+        this.price = this.horsePower *2 + (2020-this.yearsOfProduction)*1.4;
+        console.log(`The price of ${this.model} is: ${this.price} `);
+        return this.price;
     }
 }
 
-$(document).ready(function(){
 
-})
+let volvo = new Vehicles (15000, "volvo", 150, 1999);
+console.log(volvo.calculation());
+
+let calculationResult = <HTMLElement>document.getElementById("result-mb") /*as HTMLInputElement;*/
+calculationResult.innerHTML= volvo.calculation();
+
+
+
+class Motorbike extends Vehicles {
+    numberWheel= "";
+constructor (price, model, horsePower, yearsOfProduction, numberWheel ){
+    super(price, model, horsePower, yearsOfProduction);
+    this.numberWheel = numberWheel;
+}
+
+}
+
+
+/*document.getElementsByClassName("result-mb").innerHTML(`The price of ${this.model} is: ${this.price} `);*/
+
+$
+
+
 
     
